b65591116eb386058edd1c79a0a24b394c39af24
ImperiusKin <60630805+ImperiusKin@users.noreply.github.com>
2026-07-10 22:41:01 -0500

    Update event_scripts.s

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -19,7 +19,6 @@
 #include "constants/berry.h"
 #include "constants/cable_club.h"
 #include "constants/coins.h"
-#include "constants/comparison_operators.h"
 #include "constants/contest.h"
 #include "constants/daycare.h"
 #include "constants/decorations.h"
