73a1167727bb976af81f57e131f0026bddab2bd0
BelialClover <belialclover00@gmail.com>
2026-09-07 15:09:32 -0600

    Compile Fixes

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -192,6 +192,8 @@ gStdScripts_End::
 	.include "data/maps/LittlerootTown_MaysHouse_1F/scripts.inc"
 	.include "data/maps/LittlerootTown_MaysHouse_2F/scripts.inc"
 	.include "data/maps/LittlerootTown_ProfessorBirchsLab/scripts.inc"
+	.include "data/maps/Player_House/scripts.inc"
+	.include "data/maps/Player_House_F2/scripts.inc"
 	.include "data/maps/OldaleTown_House1/scripts.inc"
 	.include "data/maps/OldaleTown_House2/scripts.inc"
 	.include "data/maps/OldaleTown_PokemonCenter_1F/scripts.inc"
